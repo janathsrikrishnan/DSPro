3ad2b4cd22f68b8c144ab26b1f3895ceac0004d5
Janath jsk <45797873+janathsrikrishnan@users.noreply.github.com>
2022-10-16 09:58:56 +0530

    started: CDLinkedList finished: CDLinkedList __init__, root

--- DSPro/List/CDLinkedList.py
+++ DSPro/List/CDLinkedList.py
@@ -13,5 +13,56 @@ class CDNode:
     def release(self):
         self.__lock.release()
 
-     
+
+class CDLinkedList:
+    
+    def __init__(self, root : CDNode = None):
+        self.__root = None
+        start = CDNode("#Start#")
+        end = CDNode("#End#")    
+        if root:
+            self.__root = root
+            self.__root.prev = start
+            self.__root = self.__root.prev
+            head = self.__root
+            while head.next:
+                head = head.next
+            head.next = end
+        else:
+            self.__root = start
+            self.__root.next = end
+
+        
+    def __getitem__(self, index) -> int:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def addAt(self, item : 'any', index : int) -> bool:
+        pass
+
+    def addFront(self, item : 'any') -> bool:
+        pass
+
+    def addEnd(self, item : 'any') -> bool:
+        pass
+        
+    def contains(self) -> bool:
+        pass
+    
+    def pop(self, index : int = -1) -> 'any':
+        pass
+
+    def remove(self, item : 'any') -> bool:
+        pass
+    
+    def root(self) -> CDNode:
+        """ return the root of the list"""
+        return self.__root
+
+
+    def tolist(self) -> list:
+        pass
+
 
